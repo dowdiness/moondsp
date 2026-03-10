427d3bc0e63913cd5441bc02cc3cbaf29179fcd9
Koji Ishimoto <koji.ishimoto@gmail.com>
2026-03-10 21:16:21 +0900

    Make serve script pick a free port

--- serve.sh
+++ serve.sh
@@ -5,6 +5,7 @@ set -eu
 root_dir="$(CDPATH= cd -- "$(dirname "$0")" && pwd)"
 build_dir="$root_dir/_build/wasm-gc/release/build"
 web_dir="$root_dir/web"
+port="${1:-8080}"
 
 mkdir -p "$web_dir"
 
@@ -19,5 +20,29 @@ fi
 cp "$wasm_path" "$web_dir/moonbit_dsp.wasm"
 echo "Copied $(basename "$wasm_path") to $web_dir/moonbit_dsp.wasm"
 
+find_free_port() {
+  python3 - "$1" <<'PY'
+import socket
+import sys
+
+port = int(sys.argv[1])
+
+while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        sock.bind(("127.0.0.1", port))
+    except OSError:
+        port += 1
+    else:
+        sock.close()
+        print(port)
+        break
+PY
+}
+
+port="$(find_free_port "$port")"
+echo "Serving web demo at http://127.0.0.1:$port"
+
 cd "$web_dir"
-python3 -m http.server 8080
+python3 -m http.server "$port" --bind 127.0.0.1
